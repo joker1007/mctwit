4fee523c072f0c3e2bdf0f9df0ff054a33f2fe08
joker1007 <kakyoin.hierophant@gmail.com>
2011-09-05 22:16:19 +0900

    mixi_login, get_community_event_links, get_event

--- mixi_agent.rb
+++ mixi_agent.rb
@@ -1,9 +1,47 @@
+#coding: utf-8
 require "rubygems"
 require "bundler/setup"
 require "mechanize"
+require "kconv"
 
 class MixiAgent < Mechanize
-  def mixi_login
-    puts "test"
+  def mixi_login(login, password)
+    get "http://mixi.jp/home.pl"
+    login_form = page.form_with(:name => "login_form")
+    login_form.field_with(:name => "email").value = login
+    login_form.field_with(:name => "password").value = password
+    submit(login_form)
+    get "http://mixi.jp/home.pl"
   end
+
+  def get_community_event_links(cid)
+    get "http://mixi.jp/view_community.pl?id=#{cid}"
+    links = page.parser.css("#newCommunityEvent .contents dd a")
+  end
+
+  def get_event(link)
+    get link
+    parser = page.parser
+    mixi_event = MixiEvent.new
+    mixi_event.link = link
+    event_id, cid = link.scan(/id=(\d+).*comm_id=(\d+)/).flatten
+    mixi_event.event_id = event_id.to_i
+    mixi_event.cid = cid.to_i
+
+    title_node = parser.css("span.title").first
+    mixi_event.title = title_node.content
+
+    date_node = parser.css("span.date").first
+    year, month, day, hour, min = date_node.content.scan(/(\d{4})[^\d]*(\d{2})[^\d]*(\d{2})[^\d]*(\d{2}):(\d{2})/).flatten
+    mixi_event.created_at = Time.local(year, month, day, hour, min)
+
+    body_node = parser.css(".bbsList01 .bbsContent dd").first
+    mixi_event.body = body_node.content
+
+    mixi_event
+  end
+end
+
+class MixiEvent
+  attr_accessor :title, :body, :link, :created_at, :event_id, :cid
 end
--- spec/mixi_agent_spec.rb
+++ spec/mixi_agent_spec.rb
@@ -2,14 +2,39 @@
 require "rubygems"
 require "bundler/setup"
 require "rspec"
+require "pit"
+require "kconv"
 
 require File.join(File.dirname(__FILE__), "../mixi_agent")
+
+CONFIG = Pit.get("mctwit", :require => {:username => "Mixi Account", :password => "Mixi Password", :cid => "Community ID"})
 
 describe MixiAgent do
   let(:agent) {MixiAgent.new}
 
-  it "#mixi_login(id, pass)でログインできる" do
-    agent.mixi_login
-    agent.page.body.should =~ /Jomang/
+  it "#mixi_login(id, pass)" do
+    agent.mixi_login(CONFIG[:username], CONFIG[:password])
+    agent.page.body.toutf8.should =~ /ログアウト/
   end
+
+  context "Mixi logined" do
+    before do
+      agent.mixi_login(CONFIG[:username], CONFIG[:password])
+    end
+
+    it "#get_community_event_list(cid)" do
+      events = agent.get_community_event_links CONFIG[:cid]
+      events.should have(7).items
+      events.each do |e|
+        e["href"].should =~ /view_event\.pl/
+      end
+    end
+
+    it "#get_event(link)" do
+      events = agent.get_community_event_links CONFIG[:cid]
+      e = agent.get_event events[0]["href"]
+      p e
+    end
+  end
+
 end
